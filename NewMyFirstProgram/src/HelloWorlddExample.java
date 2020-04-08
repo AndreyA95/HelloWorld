26f7aa7cf742a061eff9574501348dcc0513bb6b
AndreyA95 <andrey.andreev95@gmail.com>
2020-04-09 10:57:30 +1200

    Deleted comment and tidy up code

--- NewMyFirstProgram/src/HelloWorlddExample.java
+++ NewMyFirstProgram/src/HelloWorlddExample.java
@@ -1,8 +1,8 @@
 
 public class HelloWorlddExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello world");
 	}
 
